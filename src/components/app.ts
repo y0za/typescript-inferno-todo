1722da329195c797eb7a5a0def682edacc582de6
y0za <yoza.is12s@gmail.com>
2017-03-23 00:21:17 +0900

    Add Input Component to App Component

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,10 +1,26 @@
 import Component from 'inferno-component'
 import h from 'inferno-hyperscript'
+import Input from './input.ts'
 
 interface Props {}
+interface State {
+  titles: string[]
+}
 
-export default class App extends Component<Props, {}> {
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      titles: []
+    }
+  }
+
+  addTitle(title: string) {
+    this.state.titles.push(title)
+    console.log(this.state.titles)
+  }
+
   render() {
-    return h('p', 'hello')
+    return h(Input, { add: this.addTitle.bind(this) })
   }
 }
